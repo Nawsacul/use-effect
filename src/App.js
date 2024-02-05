d6ddc746fc5889969263c248a053cd350ece0b2b
Lucas Queiroz <lucas.queiroz@fda.ufal.br>
2024-02-04 21:56:54 -0300

    Fetch with useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [racas, setRacas] = useState([]);
+  const [busca, setBusca] = useState('');
+
+  const fetchApi = (query) => {
+    fetch('http://localhost:8080/doguinhos' + query)
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setRacas(dados)
+      })
+  }
+
+  useEffect(() => {
+    fetchApi('');
+  }, []) // com o [] como segundo parametro, só vai ser gerado uma vez
+
+  useEffect(() => {
+    if (busca && busca.length > 3) {
+      fetchApi('?nome=' + busca);
+    }
+
+    if (busca.length === 0) {
+      fetchApi('')
+    }
+  }, [busca]) //todas as vezes que a busca mudar, ele vai fazer o fetch buscando o dado
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bem-vindo aos doguinhos!</h1>
+      <h4>Confira abaixo uma lista de raças dos doguinhos</h4>
+      <input placeholder='Buscar por Raça' onChange={evento => setBusca(evento.target.value)} />
+      <ul>
+        {racas.map(raca => <li key={raca.id}>{raca.nome}</li>)}
+      </ul>
     </div>
   );
 }
